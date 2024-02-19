acbce5c50cc01448c084570322292ff5dcbada16
paulohfsantos <paulohfsantos1@gmail.com>
2024-02-19 17:29:16 -0300

    refactor: entities Domain.Category, Product and User

--- Ecommerce.Domain/Entities/Product.cs
+++ Ecommerce.Domain/Entities/Product.cs
@@ -1,14 +1,36 @@
-﻿namespace EcommerceNet.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceNet.Domain.Entities
 {
     public class Product
     {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Description { get; set; }
-        public decimal Price { get; set; } // decimal or bigInt?
-        public decimal PriceCeiling { get; set; }
-        public decimal PriceFloor { get; set; }
+
+        public decimal Price { get; set; } = 0;
+
+        public decimal PriceCeiling { get; set; } = 0;
+
+        public decimal PriceFloor { get; set; } = 0;
+
+        [Required]
         public int StockQuantity { get; set; }
-        public Category Category { get; set; }
+
+        [Required]
+        public Category Category { get; set; } // handle this later
+
+        [Required]
+        public int CategoryId { get; set; }
     }
 }
--- Ecommerce.Domain/Entities/User.cs
+++ Ecommerce.Domain/Entities/User.cs
@@ -1,12 +1,32 @@
-﻿namespace EcommerceNet.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceNet.Domain.Entities
 {
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Username { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Firstname { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Lastname { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string Email { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public bool IsActive { get; set; }
     }
 }
