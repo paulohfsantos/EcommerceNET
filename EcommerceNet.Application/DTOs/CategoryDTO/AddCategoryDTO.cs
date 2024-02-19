ecfc0a8e5a007c5d5cc22d92ae2b825ae40bbc33
paulohfsantos <paulohfsantos1@gmail.com>
2024-02-19 17:32:20 -0300

    add: DTOs, update: Application.csproj

---
+++ EcommerceNet.Application/DTOs/CategoryDTO/AddCategoryDTO.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceNet.Application.DTOs.CategoryDTO
+{
+    public class AddCategoryDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; }
+    }
+}
---
+++ EcommerceNet.Application/DTOs/ProductDTO/AddProductDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcommerceNet.Application.DTOs.ProductDTO
+{
+    public class AddProductDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int CategoryId { get; set; } // cant have more than one category
+    }
+}
