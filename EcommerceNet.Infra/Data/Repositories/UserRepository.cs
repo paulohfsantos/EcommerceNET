1a6db4e0d68a26343d7d7e5bf529d8cb18762338
paulohfsantos <paulohfsantos1@gmail.com>
2024-02-19 17:30:43 -0300

    add: User repository class

--- EcommerceNet.Infra/Data/Repositories/UserRepository.cs
+++ EcommerceNet.Infra/Data/Repositories/UserRepository.cs
@@ -1,8 +1,35 @@
-﻿using EcommerceNet.Domain.Interfaces;
+﻿using EcommerceNet.Domain.Entities;
+using EcommerceNet.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceNet.Infra.Data.DatabaseContext
 {
     public class UserRepository: IUserRepository
     {
+        private readonly DbContext _context;
+
+        public UserRepository(DbContext context)
+        {
+            _context = context;
+        }
+
+        public async User GetProfile(int id)
+        {
+            return _context.Find<User>(id) ?? throw new Exception("User not found");
+        }
+
+        public Task<User> UpdateUser(User user)
+        {
+            var userToUpdate = _context.Find<User>(user.Id) ?? throw new Exception("User not found");
+            userToUpdate.Firstname = user.Firstname;
+            userToUpdate.Lastname = user.Lastname;
+
+            return userToUpdate;
+
+        }
+
+        public Task<User> DeactivateUser(int userId)
+        {
+        }
     }
 }
