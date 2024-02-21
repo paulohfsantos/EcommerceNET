57eec87803caebeab8a18cfd910c0aa33bb4fa42
paulohfsantos <paulohfsantos1@gmail.com>
2024-02-21 20:29:29 -0300

    add: config database relationships

--- EcommerceNet.Infra/Data/DatabaseContext/DatabaseContext.cs
+++ EcommerceNet.Infra/Data/DatabaseContext/DatabaseContext.cs
@@ -1,4 +1,5 @@
-﻿using EcommerceNet.Domain.Entities;
+﻿using Ecommerce.Domain.Entities;
+using EcommerceNet.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace EcommerceNet.Infra.Data.DatabaseContext
@@ -13,9 +14,27 @@ namespace EcommerceNet.Infra.Data.DatabaseContext
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<CartItem>()
+                .HasOne(ci => ci.Cart)
+                .WithMany(c => c.Items)
+                .HasForeignKey(ci => ci.Cart.Id);
+
+            modelBuilder.Entity<CartItem>()
+               .HasOne(ci => ci.Product)
+               .WithMany()
+               .HasForeignKey(ci => ci.Product.Id)
+               .IsRequired();
+
+            modelBuilder.Entity<Cart>()
+                .HasMany(c => c.Items)
+                .WithOne(ci => ci.Cart)
+                .HasForeignKey(ci => ci.Cart.Id);
+
             modelBuilder.Entity<Product>()
                 .HasOne(p => p.Category)
                 .WithMany(c => c.Products)
